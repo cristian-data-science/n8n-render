c29cd1c6c46b8d68e77886e62c1fec767cd0fb46
Cristian Gutiérrez <57298110+cristian-data-science@users.noreply.github.com>
2025-01-03 20:54:42 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from typing import Dict
-
-app = FastAPI()
-
-@app.get("/")
-def hello() -> Dict[str, str]:
-    return {"message": "Hola desde FastAPI (en el mismo contenedor de n8n)"}
